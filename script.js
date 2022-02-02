2e69e162e09dfbf2214f25e09955f3e346be5ea7
Maren Heck <marenjheck@gmail.com>
2022-02-01 22:35:04 -0500

    almost done

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-/* 0 is rock, 1 is paper, 2 is scissors */
-const computerPlay = function() {
+let wins = 0;
+let ties = 0;
+let losses = 0;
+const buttons = document.querySelectorAll('.button-container>button');
+
+function computerPlay() {
     switch (getRandomInt(0,3)) {
         case 0:
             return "rock";
@@ -11,71 +15,106 @@ const computerPlay = function() {
 }
 
 /* 0 for tie, 1 for win, 2 for lose */
-const playRound = function(playerSelection, computerSelection) {
+const playRound = function(playerSelection) {
+
+    let computerSelection = computerPlay();
+    playerMove = document.querySelector('.playerMove');
+    computerMove = document.querySelector('.computerMove');
+    console.log("play Round");
     playerSelection = playerSelection.toLowerCase();
     let gameState = 0;
-    if (playerSelection == computerSelection) {return 0};
+    if (playerSelection == computerSelection) {
+        if (playerSelection == 'paper') {
+            playerMove.innerHTML = '<img src="paper.png" alt="Image of paper" width="200">';
+            computerMove.innerHTML = '<img src="paper.png" alt="Image of paper" width="200">';
+        } else if (playerSelection == 'scissors') {
+            playerMove.innerHTML = '<img src="scissors.png" alt="Image of a scissors" width="200">';
+            computerMove.innerHTML = '<img src="scissors.png" alt="Image of scissors" width="200">';
+        } else {
+            playerMove.innerHTML = '<img src="rock.png" alt="Image of a rock" width="200">';
+            computerMove.innerHTML = '<img src="rock.png" alt="Image of a rock" width="200">';
+        }
+        
+        return 0;
+    };
     switch (playerSelection) {
         case "rock":
-            if (computerSelection === "paper") {gameState = 2;}
-            else {gameState = 1;}
+            playerMove.innerHTML = '<img src="rock.png" alt="Image of a rock" width="200">';
+            if (computerSelection === "paper") {
+                gameState = 2;
+                computerMove.innerHTML = '<img src="paper.png" alt="Image of paper" width="200">';
+            }
+            else {
+                gameState = 1;
+                computerMove.innerHTML = '<img src="scissors.png" alt="Image of scissors" width="200">';
+            }
             break;
         case "paper":
-            if (computerSelection === "rock") {gameState = 1;}
-            else {gameState = 2;}
+            playerMove.innerHTML = '<img src="paper.png" alt="Image of paper" width="200">';
+            if (computerSelection === "rock") {
+                gameState = 1;
+                computerMove.innerHTML = '<img src="rock.png" alt="Image of a rock" width="200">';
+            }
+            else {
+                gameState = 2;
+                computerMove.innerHTML = '<img src="scissors.png" alt="Image of scissors" width="200">';
+            }
             break;
         case "scissors":
-            if (computerSelection === "rock") {gameState = 2;}
-            else {gameState = 1}
+            playerMove.innerHTML = '<img src="scissors.png" alt="Image of a scissors" width="200">';
+            if (computerSelection === "rock") {
+                gameState = 2;
+                computerMove.innerHTML = '<img src="rock.png" alt="Image of a rock" width="200">';}
+            else {
+                gameState = 1;
+                computerMove.innerHTML = '<img src="paper.png" alt="Image of paper" width="200">';
+            }
             break;
         default:
             return -1;
-            break;
     }
-
     return gameState;
         
 }
 
-function game() {
-    console.log("Are you ready to play some rock, paper, scissors?");
-    
-    console.log("Let's begin!");
-    
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    const winText = document.querySelector('.wins');
+    const tieText = document.querySelector('.ties');
+    const lossText = document.querySelector('.losses');
 
-    let winNum = 0;
-    let roundNum = 1;
-    let ties = 0;
-    let losses = 0;
+    playerChoice = button.className;
+    let result = playRound(playerChoice);
+    if (result == 0) {
+        ties++;
+    } else if (result == 1) {
+        wins++;
+    } else if (result == 2) {
+        losses++;
+    }
+    tieText.textContent = `Ties: ${ties}`;
+    winText.textContent = `Wins: ${wins}`;
+    lossText.textContent = `Losses: ${losses}`;
+    if (losses >= 5 || wins >= 5) {
+        if (losses > wins) {
+            tieText.textContent = `You lost! :( The computer reached 5 first...`;
+        } else {
+            tieText.textContent = `You won! :) You reached 5 first!`;
+        }
+        winText.textContent = ``;
+        winText.style.cssText = 'background-color: #EEE9E3';
+        lossText.textContent = ``;
+        lossText.style.cssText = 'background-color: #EEE9E3';
+        wins = 0;
+        ties = 0;
+        losses = 0;
+    } else {
+        winText.style.cssText = 'background-color: #d6bfa865';
+        lossText.style.cssText = 'background-color: #d6bfa865';
+    }
+    console.log(`returned ${result}`);
 
-    console.log(`Let's begin round ${roundNum}`);
-    
-    let playerSelection = window.prompt("Rock, paper, or scissors?");     
-    let computerSelection = computerPlay();
-    console.log(`Player selection: ${playerSelection}; Computer Selection: ${computerSelection}`);
-    switch (playRound(playerSelection, computerSelection)) {
-        case 0:
-            console.log(`It is a tie! You both played ${playerSelection}`);
-            ties++;
-            break;
-        case 1:
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-            winNum++;
-            break;
-        case 2:
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-            losses++;
-            break;
-        case -1:
-            console.log("There was an error. Please enter a valid number.");
-            i--;
-            continue;
-    console.log(`You have ${winNum} wins.`);
+}));
 
-    console.log("The five games are over.");
-    console.log(`You had a total of ${winNum} wins, ${ties} ties, and ${losses} losses.`);
-    
-}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -83,4 +122,6 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
-game();
+function wait() {
+    setTimeout(() => {  console.log("Waiting 1000ms!"); }, 1000);
+}
